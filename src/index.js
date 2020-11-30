fa891ac92bf06bf622ca6519678a749e84e827b7
Charbel t <charbel2000.52@gmail.com>
2020-11-30 14:22:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function renderAllImages(data) {
 function loadImages() {
   // when the page loads
   // make a GET request
-  
+  const ImgUrl = 
   fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(response => response.json())
     .then(renderAllImages)
